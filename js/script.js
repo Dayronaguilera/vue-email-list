5792166d5284c42ef8fcafdaedb1ea10e549aae8
Dayronaguilera <aguilera.dayron@gmail.com>
2021-07-08 17:13:50 +0200

    Prova Bonus

--- js/script.js
+++ js/script.js
@@ -4,8 +4,35 @@ new Vue(
     {
         el: '#app',
         data: {
+            emails:[
+
+            ],//array vuoto che verra popolato dalla funzione!
+            check: false,
+        },
+        created(){
+            this.getemail();// richiamando la funzione colleghiamo la chiamata con array
+            //altrimenti possiamo creare un bottone dove al click inseriremo modifichermo il dato che verra eseguio nel html!
+            this.arrayFull();
         },
         methods: {
+            getemail: function () {
+                for (let x = 0; x < 10; x++) {
+                    
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')// inseriamo l'indirizzo al quale faremo la chiamata!
+                    .then((response) => {  // appena il server ci risponde, si attiva questa funzione
+                        this.emails.push(response.data.response);//pusha cio che abbiamo chiesto nella chiamata!
+                        //ps: nn e ancora visibile dal array "array e vuoto"
+                        
+                    });
+                };
+            },
+            arrayFull: function () {
+                if (this.emails.length > 11) {
+                    this.check = false;
+                } else {
+                    this.check = true;
+                }
+            }
         }    
     });
 
